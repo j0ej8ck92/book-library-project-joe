d52f5c6516538ed1e506b0c7d1887d8ccad62b8d
j0ej8ck92 <j0ej8ck92@gmail.com>
2026-05-01 06:34:21 +0000

    back to jscript

---
+++ main.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkiein", 300, "yes");
+addBookToLibrary("Bless Me Ultima", "Rudolfo Amaya", 200, "yes");
+
+
+console.log(myLibrary);
+
+
